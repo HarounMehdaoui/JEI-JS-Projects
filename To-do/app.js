1fe33ad4aa2ac3b568ae7609cc863a428c7078a4
HarounMehdaoui <mehdaoui.haroun.edu@gmail.com>
2025-12-07 00:46:45 +0100

    first commit

---
+++ To-do/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => 
+    {
+        const taskInput = document.getElementById('taskinput');
+        const addTaskBtn = document.getElementById('add-task-btn');
+        const tasklist = document.getElementById('task-list');
+        
+    }
+)
